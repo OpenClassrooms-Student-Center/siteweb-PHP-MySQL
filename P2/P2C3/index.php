bf2a402c739367aeb5abf9d0d375b76f9746e155
sthnbrs <sthbrs@free.fr>
2023-11-25 09:42:11 +0100

    [feat] code source for P2

---
+++ P2/P2C3/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'author' => 'mickael.andrieu@exemple.com',
+    ],
+    [
+        'title' => 'Couscous',
+        'author' => 'mickael.andrieu@exemple.com',
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'author' => 'mathieu.nebra@exemple.com',
+    ],
+    [
+        'title' => 'Salade Romaine',
+        'author' => 'laurene.castor@exemple.com',
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Affichage des recettes</title>
+</head>
+<body>
+    <ul>
+        <?php for ($lines = 0; $lines <= 1; $lines++) : ?>
+            <li><?php echo $recipes[$lines]['title'] . ' (' . $recipes[$lines]['author'] . ')'; ?></li>
+        <?php endfor; ?>
+    </ul>
+</body>
+</html>
---
+++ P2/P2C6/ErrorsVersion/index.php
@@ -0,0 +1,98 @@
+<?php
+
+$users = [
+    [
+        'full_name' => 'Mickaël Andrieu',
+        'email' => 'mickael.andrieu@exemple.com'
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Mathieu Nebra',
+        'email' => 'mathieu.nebra@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+    ],
+];
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Salade Romaine',
+        'recipe' => 'Etape 1 : prenez une belle salade',
+        'author' => 'lorene.castor@exemple.com',
+        'is_enablad' => true,
+    ],
+];
+
+function displayAuthor(string $authorEmail, array $users): string
+{
+    foreach ($users as $user) {
+        if ($authorEmail === $user['email']) {
+            return $user['full_name'] . '(' . $user['age'] . ' ans)';
+        }
+    }
+}
+
+function isValidRecipe(array $recipe): bool
+{
+    return $recipe['is_enabled'];
+}
+function getRecipes(array $recipes): array
+{
+    $valid_recipes = [];
+
+    foreach ($recipes as $recipe) {
+        if (isValidRecipe($recipe)) {
+            $valid_recipes[] = $recipe;
+        }
+    }
+
+    return $valid_recipes;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Recettes de cuisine : Erreur(</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+    >
+</head>
+<body>
+    <div class="container">
+        <h1>Liste des recettes de cuisine</h1>
+
+        <?php foreach (getRecipes($recipes) as $recipe) : ?>
+            <article>
+                <h3><?php echo($recipe['title']); ?></h3>
+                <div><?php echo($recipe['recipe']); ?></div>
+                <i><?php echo(displayAuthor($recipe['author'], $users)); ?></i>
+            </article>
+        <?php endforeach ?>
+    </div>
+</body>
+</html>
+
